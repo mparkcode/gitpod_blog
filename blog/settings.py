dd1081f08345750020eaa46e0a08f54b35786e5d
Michael Park <mparkcode@gmail.com>
2019-08-23 09:19:34 +0000

    collect static

--- blog/settings.py
+++ blog/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
-STATIS_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
